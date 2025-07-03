e6d24f99e70cbf3105a0db2acc62aa25f8b9040f
zuyatna <zuyatna@gmail.com>
2025-07-04 06:30:17 +0700

    adding movie model

--- backend/internal/model/movie.go
+++ backend/internal/model/movie.go
@@ -1,13 +1,20 @@
 package model
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Movie struct {
-	ID          int     `json:"id" gorm:"primaryKey"`
-	Title       string  `json:"title" gorm:"not null"`
-	Description string  `json:"description"`
-	Year        int     `json:"year"`
-	Rating      float64 `json:"rating"`
-	Poster      []byte  `json:"-" gorm:"type:bytea"`
-	PosterURL   string  `json:"poster_url" gorm:"-"`
-	CreatedAt   string  `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   string  `json:"updated_at" gorm:"autoUpdateTime"`
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	Title       string         `json:"title" gorm:"not null"`
+	Description string         `json:"description" gorm:"type:text"`
+	Year        int            `json:"year" gorm:"type:integer"`
+	Rating      float64        `json:"rating" gorm:"type:float"`
+	Poster      []byte         `json:"-" gorm:"type:bytea"`
+	PosterURL   string         `json:"poster_url" gorm:"-"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
