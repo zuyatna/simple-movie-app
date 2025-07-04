6593ff40130d1690c54eba1e0d99ef36e30e7ac7
zuyatna <zuyatna@gmail.com>
2025-07-04 20:23:45 +0700

    adding auth handler

---
+++ backend/internal/handler/auth_handler.go
@@ -0,0 +1,83 @@
+package handler
+
+import (
+	"movie-api/internal/model"
+	"movie-api/internal/usecase"
+	"movie-api/pkg/utils"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthHandler struct {
+	authUsecase usecase.AuthUsecase
+}
+
+func NewAuthHandler(authUsecase usecase.AuthUsecase) *AuthHandler {
+	return &AuthHandler{
+		authUsecase: authUsecase,
+	}
+}
+
+func (ah *AuthHandler) Register(c *gin.Context) {
+	var payload model.RegisterPayload
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := ah.authUsecase.Register(&payload)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message":  "User registered successfully",
+		"user_id":  user.ID,
+		"username": user.Username,
+	})
+}
+
+func (ah *AuthHandler) Login(c *gin.Context) {
+	var payload model.LoginPayload
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := ah.authUsecase.Login(&payload)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":       "Login successful",
+		"access_token":  token["access_token"],
+		"refresh_token": token["refresh_token"],
+	})
+}
+
+func (ah *AuthHandler) RefreshToken(c *gin.Context) {
+	var body struct {
+		RefreshToken string `json:"refresh_token"`
+	}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Refresh token is required"})
+		return
+	}
+	claims, err := utils.ValidateToken(body.RefreshToken, os.Getenv("JWT_REFRESH_SECRET_KEY"))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired refresh token"})
+		return
+	}
+	newToken, err := utils.GenerateToken(claims.UserID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate new token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, newToken)
+}
