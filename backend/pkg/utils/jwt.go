281282f6e629c39067680c351db68213442e7abc
zuyatna <zuyatna@gmail.com>
2025-07-04 18:53:24 +0700

    adding jwt

---
+++ backend/pkg/utils/jwt.go
@@ -0,0 +1,88 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type Claims struct {
+	UserID uuid.UUID `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uuid.UUID) (map[string]string, error) {
+	accessToken, err := generateAcessToken(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	refreshToken, err := generateRefreshToken(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return map[string]string{
+		"access_token":  accessToken,
+		"refresh_token": refreshToken,
+	}, nil
+}
+
+func generateAcessToken(userID uuid.UUID) (string, error) {
+	lifespan, err := strconv.Atoi(os.Getenv("ACCESS_TOKEN_LIFESPAN"))
+	if err != nil {
+		return "", err
+	}
+
+	claims := &Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * time.Duration(lifespan))),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_ACCESS_SECRET_KEY")))
+}
+
+func generateRefreshToken(userID uuid.UUID) (string, error) {
+	lifespan, err := strconv.Atoi(os.Getenv("REFRESH_TOKEN_LIFESPAN"))
+	if err != nil {
+		return "", err
+	}
+
+	claims := &Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * time.Duration(lifespan))),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_REFRESH_SECRET_KEY")))
+}
+
+func ValidateToken(tokenString string, secretKey string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, fmt.Errorf("invalid token")
+}
