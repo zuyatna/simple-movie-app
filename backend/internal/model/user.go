60b27257836518d907dc64a80571356590c1ab9e
zuyatna <zuyatna@gmail.com>
2025-07-04 21:47:40 +0700

    fix password field in postgresql data type

--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -9,7 +9,7 @@ import (
 type User struct {
 	ID        uuid.UUID `json:"id" gorm:"type:uuid; primaryKey; default:gen_random_uuid()"`
 	Username  string    `json:"username" gorm:"type:varchar(20); unique; not null"`
-	Password  string    `json:"password" gorm:"type:varchar(20); not null"`
+	Password  string    `json:"password" gorm:"not null"`
 	Role      string    `json:"role" gorm:"type:varchar(10); default:'user'"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
