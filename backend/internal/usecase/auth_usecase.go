b1691e67bc2958a583ab18be1f541d120e9ae6ce
zuyatna <zuyatna@gmail.com>
2025-07-04 19:51:50 +0700

    adding auth usecase

---
+++ backend/internal/usecase/auth_usecase.go
@@ -0,0 +1,58 @@
+package usecase
+
+import (
+	"errors"
+	"movie-api/internal/model"
+	"movie-api/internal/repository"
+	"movie-api/pkg/utils"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthUsecase interface {
+	Register(payload *model.RegisterPayload) (*model.User, error)
+	Login(payload *model.LoginPayload) (map[string]string, error)
+}
+
+type authUsecase struct {
+	userRepo repository.UserRepository
+}
+
+func NewAuthUsecase(userRepo repository.UserRepository) AuthUsecase {
+	return &authUsecase{
+		userRepo: userRepo,
+	}
+}
+
+func (a *authUsecase) Register(payload *model.RegisterPayload) (*model.User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &model.User{
+		Username: payload.Username,
+		Password: string(hashedPassword),
+	}
+
+	return a.userRepo.CreateUser(user.Username, user.Password)
+}
+
+func (a *authUsecase) Login(payload *model.LoginPayload) (map[string]string, error) {
+	user, err := a.userRepo.FindUserByUsername(payload.Username)
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password))
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	token, err := utils.GenerateToken(user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
